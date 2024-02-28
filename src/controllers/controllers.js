95affbdcd0e93f200bd540e95ce77407cf381201
danieldaga <daniel@arhrestauracion.com>
2024-02-28 16:12:54 +0100

    add: controller findbycolor

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,5 +1,6 @@
-const axios = require("axios");
-
+const axios = require('axios');
+const { parse } = require('json2csv');
+const fs = require('fs');
 
 const test = (req, res) => {
     res.send("testAPI")
@@ -16,17 +17,34 @@ const findByName = async (req, res) => {
         res.status(500).json({ error: 'Ocurrió un error al procesar tu solicitud.' });
     }
 }
+
+// const findByColor = async (req, res) => {
+//     try {
+//         const color = req.params.color
+//         const apiUrl = `https://pokeapi.co/api/v2/pokemon-color/${color}/`
+//         const response = await axios.get(apiUrl)
+//         res.json(response.data.pokemon_species)
+//     } catch (error) {
+//         console.error('Error al llamar a la API:', error);
+//         res.status(500).json({ error: 'Ocurrió un error al procesar tu solicitud.' });
+//     }
+// }
+
 const findByColor = async (req, res) => {
     try {
-        const name = req.params.name
-        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${name}/`
-        const response = await axios.get(apiUrl)
-        res.json(response.data)
+        const color = req.params.color;
+        const apiUrl = `https://pokeapi.co/api/v2/pokemon-color/${color}/`;
+        const response = await axios.get(apiUrl);
+        const pokemonSpecies = response.data.pokemon_species.map(pokemon => ({
+            name: pokemon.name
+        }));
+        res.json(pokemonSpecies);
     } catch (error) {
         console.error('Error al llamar a la API:', error);
         res.status(500).json({ error: 'Ocurrió un error al procesar tu solicitud.' });
     }
 }
+
 
 module.exports = {
     test,
--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -6,7 +6,7 @@ const router = Router()
 
 router.get('/pokemon/:name', findByName)
 router.get('/PRUEBA', test)
-router.get('/pokemon/:color', findByColor)
+router.get('/pokemon-color/:color', findByColor)
 
 
 module.exports = router
